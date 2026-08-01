8eb8e6d5fa1e1f43a04194d8291cba2bb2dea65a
Max <hamidzonovmuhriddin@gmail.com>
2026-08-02 08:38:19 +0900

    fix: modify routing system and controllers

---
+++ src/routerAdmin.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const routerAdmin = express.Router();
+import storeController from "./controllers/store.controller";
+
+routerAdmin.get("/", storeController.goHome);
+
+routerAdmin.get("/login", storeController.getLogin);
+
+export default routerAdmin;
