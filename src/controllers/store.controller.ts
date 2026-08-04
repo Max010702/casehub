7d2812da9535eb03ad9ff14e8cc14a2a4ccac5ab
Max <hamidzonovmuhriddin@gmail.com>
2026-08-04 19:45:50 +0900

    feat: created restaurant member related router get and post endpoints

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -7,7 +7,7 @@ const storeController: T = {};
 storeController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home  Page");
+    res.send("Home Page");
   } catch (err) {
     console.log("Error, goHome", err);
   }
@@ -16,9 +16,36 @@ storeController.goHome = (req: Request, res: Response) => {
 storeController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login   Page");
+    res.send("Login Page");
   } catch (err) {
     console.log("Error, getLogin:", err);
+  }
+};
+
+storeController.getSignup = (req: Request, res: Response) => {
+  try {
+    console.log("getSignup");
+    res.send("Signup Page");
+  } catch (err) {
+    console.log("Error, getSignup:", err);
+  }
+};
+
+storeController.processLogin = (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error, processLogin:", err);
+  }
+};
+
+storeController.processSignup = (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error, processSignup:", err);
   }
 };
 
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,8 +2,17 @@ import express from "express";
 const routerAdmin = express.Router();
 import storeController from "./controllers/store.controller";
 
+/** Store */
 routerAdmin.get("/", storeController.goHome);
+routerAdmin
+  .get("/login", storeController.getLogin)
+  .post("/login", storeController.processLogin);
+routerAdmin
+  .get("/signup", storeController.getSignup)
+  .post("/signup", storeController.processSignup);
 
-routerAdmin.get("/login", storeController.getLogin);
+/** Product */
+
+/** User */
 
 export default routerAdmin;
