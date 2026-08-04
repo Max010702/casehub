a195402fa12de7b78da2ebb8b236be367daf68f3
Max <hamidzonovmuhriddin@gmail.com>
2026-08-04 10:41:06 +0900

    feat: create project standarts

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { log } from "console";
 
 const storeController: T = {};
 storeController.goHome = (req: Request, res: Response) => {
   try {
+    console.log("goHome");
     res.send("Home  Page");
   } catch (err) {
     console.log("Error, goHome", err);
@@ -13,6 +15,7 @@ storeController.goHome = (req: Request, res: Response) => {
 
 storeController.getLogin = (req: Request, res: Response) => {
   try {
+    console.log("getLogin");
     res.send("Login   Page");
   } catch (err) {
     console.log("Error, getLogin:", err);
