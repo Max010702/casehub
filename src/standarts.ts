c5e544eddecca5a9854176bb80409a9884b38c29
Max <hamidzonovmuhriddin@gmail.com>
2026-08-04 19:51:07 +0900

    fix: router admin naming to project standarts

---
+++ src/standarts.ts
@@ -0,0 +1,10 @@
+/**
+  Project Standarts:
+  - Logging standarts
+  - Naming standarts:
+      function, method, variable => CAMEL
+      class => PASCAL
+      folder, file => KEBAB
+      css => SNAKE
+  - Error handling 
+ */
