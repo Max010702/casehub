bda8b547e8cc0e9dd82e8a73d7b9b5bed5b03dbf
Max <hamidzonovmuhriddin@gmail.com>
2026-08-23 08:02:53 +0900

    feat: create new product frontend new logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -30,7 +30,6 @@ productController.createNewProduct = async (
 ) => {
   try {
     console.log("createNewProduct");
-    console.log("req.files:", req.files);
 
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
@@ -44,14 +43,14 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert("Sucessfull creation!"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("Sucessfull creation!"); window.location.replace('/admin/product/all') </script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("Error: ${message}"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("Error: ${message}"); window.location.replace('/admin/product/all') </script>`,
     );
   }
 };
