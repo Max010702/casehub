0aa30ad2891e8bb11bc4e5a90c9db79a6162b8f6
Max <hamidzonovmuhriddin@gmail.com>
2026-08-23 20:32:05 +0900

    feat: develop updateOrder rest Api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,8 @@ import { T } from "../libs/types/common";
 import { Response } from "express";
 import { ExtendedRequest } from "../libs/types/member";
 import OrderService from "../models/Order.service";
+import { OrderInquiry, OrderUpdateInput } from "../libs/types/order";
+import { OrderStatus } from "../libs/enums/order.enum";
 
 const orderService = new OrderService();
 
@@ -36,7 +38,21 @@ orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
   } catch (err) {
     console.log("Error, getMyOrders:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standart.code).json(Errors.standart);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+orderController.updateOrder = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateOrder");
+    const input: OrderUpdateInput = req.body;
+    const result = await orderService.updateOrder(req.member, input);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, updateOrder:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
 
